2402ffdd5e376857a1849ccd253f92fd721231f7
Rein-cyber <racepgames@gmail.com>
2021-04-25 14:19:05 +0000

    second commit

---
+++ install.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Generador de Wordpress</title>
+		<style>
+			.topleft {
+			  position: absolute;
+			  top: 100px;
+			  left: 16px;
+			  font-size: 18px;
+			}
+			.topright {
+			  position: absolute;
+			  top: 100px;
+			  right: 500px;
+			  font-size: 18px;
+			}
+			.bottomleft {
+			  position: absolute;
+			  bottom: 8px;
+			  left: 16px;
+			  font-size: 18px;
+			}
+			.bottomright {
+			  position: absolute;
+			  bottom: 8px;
+			  right: 16px;
+			  font-size: 18px;
+			}
+		</style>
+	</head>
+	<body>
+		<link rel="stylesheet" href="bootstrap.min.css" />
+		<h1>&nbsp;Generador de Wordpress</h1>
+		<hr>
+
+	</body>
+</html>
+<?php
+
+
+?>
